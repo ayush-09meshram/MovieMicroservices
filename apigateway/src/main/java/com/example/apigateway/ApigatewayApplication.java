b02353da7a20c447d734029a9651ccd4cf8a2884
kushagraagrawal <kushagra106@gmail.com>
2018-10-12 18:04:09 +0530

    zuul added

--- apigateway/src/main/java/com/example/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/example/apigateway/ApigatewayApplication.java
@@ -1,14 +1,14 @@
-package com.stackroute.apigatewayzuul;
+package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
 @SpringBootApplication
-public class ApigatewayZuulApplication {
+@EnableZuulProxy
+public class ApigatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApigatewayZuulApplication.class, args);
+		SpringApplication.run(ApigatewayApplication.class, args);
 	}
 }
--- apigateway/src/test/java/com/example/apigateway/ApigatewayApplicationTests.java
+++ apigateway/src/test/java/com/example/apigateway/ApigatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.stackroute.apigatewayzuul;
+package com.example.apigateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApigatewayZuulApplicationTests {
+public class ApigatewayApplicationTests {
 
 	@Test
 	public void contextLoads() {
