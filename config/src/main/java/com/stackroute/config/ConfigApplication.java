88e35b203e086d83c9474b39d1936df8ad34fb04
kushagraagrawal <kushagra106@gmail.com>
2018-10-12 16:57:03 +0530

    almost done

--- config/src/main/java/com/stackroute/config/ConfigApplication.java
+++ config/src/main/java/com/stackroute/config/ConfigApplication.java
@@ -1,13 +1,17 @@
 package com.stackroute.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @EnableConfigServer
 @SpringBootApplication
 @EnableEurekaClient
+//@EnableDiscoveryClient
 public class ConfigApplication {
 
 	public static void main(String[] args) {
